3dc14dc9576492ff0dd3846f6ee5164f3557fc3a
kyunghwaisdev <kyunghwaisdev@gmail.com>
2022-12-16 15:21:37 +0900

    homework

---
+++ src/main/java/com/sparta/hanghae99_spring2_assignment/Hanghae99Spring2AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghae99_spring2_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanghae99Spring2AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hanghae99Spring2AssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/hanghae99_spring2_assignment/Hanghae99Spring2AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghae99_spring2_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hanghae99Spring2AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
